ca1437d625eb132e07c2f2fbb620eefe0eed2e39
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-09-06 17:15:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+
+function findMatching(drivers, name){
+    let matchingNames= drivers.filter(
+    (match) => match.toLowerCase() === name.toLowerCase()
+    )
+    return matchingNames
+}
+
+function fuzzyMatch(drivers, name) {
+    const driversArray = drivers.filter(
+        (match) => match.toLowerCase().indexOf(name.toLowerCase()) === 0
+    )
+    return driversArray;
+}
+
+function matchName(driver, name) {
+    const driversArray = driver.filter(
+        (match) => match.name === name
+    )
+    return driversArray;}
